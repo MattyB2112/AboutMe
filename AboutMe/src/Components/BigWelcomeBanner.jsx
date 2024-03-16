92dea4df4131fdab56d203def27d19abc95ad699
Matt Barker <mattbarker23@hotmail.com>
2024-03-16 15:09:14 +0000

    very basic homepage template

---
+++ AboutMe/src/Components/BigWelcomeBanner.jsx
@@ -0,0 +1,10 @@
+export default function BigWelcomeBanner() {
+  return (
+    <>
+      <div className="big-welcome-banner-container">
+        <div className="hi-there">Hi there!</div>
+        <div className="welcome-bit">Welcome to my page!</div>
+      </div>
+    </>
+  );
+}
---
+++ AboutMe/src/Components/Header.jsx
@@ -0,0 +1,17 @@
+export default function Header() {
+  return (
+    <>
+      <div className="header-container">
+        <div className="left-header-container">
+          <div className="left-header-element">Home</div>
+        </div>
+        <div className="right-header-container">
+          <div className="right-header-element">About</div>
+          <div className="right-header-element">CV</div>
+          <div className="right-header-element">Projects</div>
+          <div className="right-header-element">Contact</div>
+        </div>
+      </div>
+    </>
+  );
+}
