811d56ad135b215456084cfbaa009fe7ffa8f53a
Matt Barker <mattbarker23@hotmail.com>
2024-04-21 19:22:37 +0100

    amended home page layout and added to projects

--- AboutMe/src/Components/BigWelcomeBanner.jsx
+++ AboutMe/src/Components/BigWelcomeBanner.jsx
@@ -1,48 +1,71 @@
 import { useState, useEffect } from "react";
 import { ReactTyped } from "react-typed";
 
+import html from "../Images/html.png";
+import css from "../Images/css.png";
+import react from "../Images/react.png";
+import javascript from "../Images/javascript.png";
+
 export default function BigWelcomeBanner() {
   let cursorOn = false;
 
   return (
-    <>
-      <div className="big-welcome-banner-container">
-        <div className="hi-there">
-          <em>
-            <ReactTyped
-              strings={["Hello there!\n", "I am a &#32"]}
-              typeSpeed={50}
-              backSpeed={50}
-              showCursor={false}
-            />
-          </em>
-          {/* <em>
-            <div>{"\n"}</div>
-            <ReactTyped
-              strings={["I am a &#32\n"]}
-              typeSpeed={50}
-              backSpeed={180}
-              startDelay={1900}
-              showCursor={false}
-            />
-          </em> */}
-          <em>
-            <ReactTyped
-              strings={[
-                "proggrammer!\n",
-                "progammer!\n",
-                "programer!\n",
-                "coder!",
-              ]}
-              typeSpeed={50}
-              backSpeed={50}
-              startDelay={3000}
-              backDelay={500}
-              showCursor={true}
-            />
-          </em>
+    <div className="home-container">
+      <div className="welcome-banner-container">
+        <div className="typed-container">
+          <>
+            Hi! I'm Matt
+            <br />|
+          </>
+
+          <ReactTyped
+            strings={["I'm a &#32"]}
+            typeSpeed={50}
+            backSpeed={50}
+            showCursor={false}
+          />
+          <ReactTyped
+            strings={[
+              "proggrammer!\n",
+              "progammer!\n",
+              "programer!\n",
+              "coder!",
+            ]}
+            typeSpeed={50}
+            backSpeed={50}
+            startDelay={700}
+            backDelay={500}
+            showCursor={true}
+          />
+        </div>
+        <div className="welcome-text">
+          Recent graduate of the Northcoders Software Development bootcamp, with
+          specializations in HTML, JavaScript, CSS, Node.js, SQL, Test-Driven
+          Development, and React, but also experience with React Native and
+          MongoDB.
         </div>
       </div>
-    </>
+      <div className="language-logos-container">
+        <div className="language-logos-container-top">
+          <div className="html">
+            <img src={html} className="html" />
+          </div>
+          <div className="css">
+            <img src={css} className="css" />
+          </div>
+          <div className="javascript">3</div>
+        </div>
+        <div className="language-logos-container-mid">
+          <div className="html">1</div>
+          <div className="css">2</div>
+          <div className="javascript">3</div>
+        </div>
+        <div className="language-logos-container-bottom">
+          <div className="html">1</div>
+          <div className="css">2</div>
+          <div className="javascript">3</div>
+        </div>
+      </div>
+    </div>
   );
 }
--- AboutMe/src/Components/Projects.jsx
+++ AboutMe/src/Components/Projects.jsx
@@ -1,30 +1,33 @@
 import imageOne from "../Images/n-and-n-pic.jpg";
+import imageTwo from "../Images/trippin1.png";
+import imageThree from "../Images/trippin2.png";
 
 export default function Projects() {
   return (
     <>
       <div className="projects-container">
-        <div className="project-container">
-          <div className="project-image">
-            <a href="https://networkingandnews.netlify.app/">
-              <img src={imageOne} className="project-image" />
-            </a>
-          </div>
-          <div className="project-info">
-            This was a project undertaken during my time on the Northcoders
-            software development bootcamp. I first created the API endpoints
-            that would store the data to be fetched by the frontend, and used
-            Express to create a server for these. The data was stored in SQL and
-            queries to the database were made using Postgres.
+        <div className="ncnews-container">
+          <a href="https://networkingandnews.netlify.app/">
+            <img src={imageOne} className="ncnews-image" />
+          </a>
+
+          <div className="ncnews-info">
+            News article-style website - Made during my course. Article data is
+            stored in a SQL database. Endpoints were created to make queries to
+            this data using Postgres. Frontend was coded in React and uses Axios
+            to make calls to the API.
           </div>
         </div>
-        <div className="project-container">
-          <div className="project-image">Project 2 Title</div>
-          <div className="project-info">Project 2 info</div>
-        </div>
-        <div className="project-container">
-          <div className="project-image">Project 3 Title</div>
-          <div className="project-info">Project 3 info</div>
+        <div className="trippin-container">
+          <div className="trippin-images">
+            <img src={imageThree} className="trippin-image" />
+            <img src={imageTwo} className="trippin-image" />
+          </div>
+          <div className="trippin-info">
+            Trippin - Trip-planning app made for final project of my course.
+            Backend database was created using MongoDB. Frontend was created
+            using React Native.
+          </div>
         </div>
       </div>
     </>
