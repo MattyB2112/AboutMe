066e5daf1f3e5fabea37e6069806628705561f05
Matt Barker <mattbarker23@hotmail.com>
2024-04-27 13:20:30 +0100

    change sizing of project containers

--- AboutMe/src/Components/Projects.jsx
+++ AboutMe/src/Components/Projects.jsx
@@ -10,11 +10,27 @@ export default function Projects() {
           <div className="ncnews-underlay"></div>
           <div className="ncnews-overlay">
             <br />
-            News article-style website - Made during my course. Article data is
-            stored in a SQL database. Endpoints were created to make queries to
-            this data using Postgres. Frontend was coded in React and uses Axios
-            to make calls to the API. Articles can be filtered by topic, and
-            sorted by number of votes and date posted.
+            Networking and news - Made during my course. Article data is stored
+            in a SQL database. Endpoints were created to make queries to this
+            data using Postgres. Frontend was coded in React and uses Axios to
+            make calls to the API. Articles can be filtered by topic, and sorted
+            by number of votes and date posted.
+          </div>
+        </div>
+        <div className="trippin-container">
+          <div className="trippin-underlay"></div>
+          <div className="trippin-images">
+            <img src={imageThree} className="trippin-image" />
+            <img src={imageTwo} className="trippin-image" />
+          </div>
+          <div className="trippin-overlay">
+            <br />
+            Trippin - Trip-planning app made for the final project of my course.
+            Backend database was created using MongoDB. Frontend was created
+            using React Native. Users create an upcoming trip with a name, start
+            and end-date, and then can add travel, stay, and activity info.
+            Members can be added by any users that have been added to the trip
+            but can only removed by the trip admin.
           </div>
         </div>
         <div className="trippin-container">
